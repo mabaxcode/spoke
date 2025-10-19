74f3ff2a20261bf82c01918cbb1708b4a4bf552f
MABA <hayabusaxgs@gmail.com>
2025-10-20 07:35:24 +0800

    fixing

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,7 +13,7 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin/dashboard');
+		$this->load->view('admin/answers');
 	}
 
     public function answers()
